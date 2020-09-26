5e204632d5b255bf86ef5af51ab7fd56b2c864c7
Tobias Theel <tt@fino.digital>
2020-09-26 17:31:51 +0200

    add fizzbuzz as function example

---
+++ ch1/functions/main.go
@@ -0,0 +1,58 @@
+package main
+
+import "errors"
+
+func main() {
+	result, err := FizzBuzz(15)
+	if err != nil {
+		println(err.Error())
+	}
+
+	println(result)
+
+	result, err = FizzBuzz(3)
+	if err != nil {
+		println(err.Error())
+	}
+
+	println(result)
+
+	result, err = FizzBuzz(5)
+	if err != nil {
+		println(err.Error())
+	}
+
+	println(result)
+
+	result, err = FizzBuzz(4)
+	if err != nil {
+		println(err.Error())
+	}
+
+	println(result)
+
+	_, err = FizzBuzz(-15)
+	if err != nil {
+		println(err.Error())
+	}
+}
+
+func FizzBuzz(number int) (string, error) {
+	if number < 0 {
+		return "", errors.New("negative numbers are not allowed")
+	}
+
+	if number%3 == 0 && number%5 == 0 {
+		return "FizzBuzz", nil
+	}
+
+	if number%3 == 0 {
+		return "Fizz", nil
+	}
+
+	if number%5 == 0 {
+		return "Buzz", nil
+	}
+
+	return "", nil
+}
