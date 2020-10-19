a8fd09ac8aa50b4ad085ae73e3735a4c31b341d0
Tobias Theel <tt@fino.digital>
2020-10-19 22:44:13 +0200

    add hello-world-of-things and add blink-sos

---
+++ Makefile
@@ -0,0 +1,2 @@
+hello-world-of-things: 
+	tinygo flash --target=arduino ch2/hello-world-of-things/main.go
---
+++ ch2/blink-sos/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"machine"
+	"time"
+)
+
+func main() {
+	led := machine.Pin(5)
+	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	for {
+		blink3(led, 500, 350)
+
+		blink3(led, 3000, 350)
+
+		blink3(led, 500, 350)
+	}
+}
+
+func blink3(led machine.Pin, duration, lowDuration time.Duration) {
+	for i := 0; i < 3; i++ {
+		led.High()
+		time.Sleep(time.Millisecond * duration)
+		led.Low()
+		time.Sleep(time.Millisecond * lowDuration)
+	}
+}
