4b03dfc61796353fff07d7fa75198b5b58bf9829
Tobias Theel <tt@fino.digital>
2020-11-04 21:21:31 +0100

    chapter 2 projects

--- ch2/light-button-floating/main.go
+++ ch2/light-button-floating/main.go
@@ -4,9 +4,14 @@ import (
 	"machine"
 )
 
+// This example is used to show floating of the buttons value, when no Pull-Up resistor is being used
+// The button state is printed to serial, in order to watch the effect
 func main() {
 	outputConfig := machine.PinConfig{Mode: machine.PinOutput}
 	inputConfig := machine.PinConfig{Mode: machine.PinInput}
+
+	// To get around this PinInputPullUp can be used. If the pin is configured with PinInputPullup an internal resistor is being used.
+	// inputConfig := machine.PinConfig{Mode: machine.PinInputPullup}
 
 	led := machine.D13
 	led.Configure(outputConfig)
---
+++ ch2/traffic-lights-pedestrian/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"machine"
+	"time"
+)
+
+var stopTraffic bool
+
+// It is important to set a scheduler for this to work
+// tinygo flash -scheduler tasks -target=arduino ch2/traffic-lights-pedestrian/main.go
+func main() {
+	stopTraffic = false
+
+	outputConfig := machine.PinConfig{Mode: machine.PinOutput}
+
+	greenLED := machine.D13
+	greenLED.Configure(outputConfig)
+
+	yellowLED := machine.D12
+	yellowLED.Configure(outputConfig)
+
+	redLED := machine.D11
+	redLED.Configure(outputConfig)
+
+	pedestrianGreen := machine.D4
+	pedestrianGreen.Configure(outputConfig)
+	pedestrianRED := machine.D5
+	pedestrianRED.Configure(outputConfig)
+
+	inputConfig := machine.PinConfig{Mode: machine.PinInput}
+	buttonInput := machine.D2
+	buttonInput.Configure(inputConfig)
+
+	pedestrianRED.High()
+	pedestrianGreen.Low()
+
+	go trafficLights(redLED, greenLED, yellowLED, pedestrianRED, pedestrianGreen)
+
+	for {
+		if buttonInput.Get() {
+			stopTraffic = true
+		}
+
+		time.Sleep(50 * time.Millisecond)
+	}
+}
+
+func trafficLights(redLED, greenLED, yellowLED, pedestrianRED, pedestrianGreen machine.Pin) {
+	for {
+		if stopTraffic {
+			redLED.High()
+			yellowLED.Low()
+			greenLED.Low()
+
+			pedestrianGreen.High()
+			pedestrianRED.Low()
+			time.Sleep(3 * time.Second)
+
+			pedestrianGreen.Low()
+			pedestrianRED.High()
+
+			stopTraffic = false
+		} else {
+			pedestrianGreen.Low()
+			pedestrianRED.High()
+		}
+
+		redLED.High()
+		time.Sleep(time.Second)
+		yellowLED.High()
+		time.Sleep(time.Second)
+		redLED.Low()
+		yellowLED.Low()
+		greenLED.High()
+		time.Sleep(time.Second)
+
+		greenLED.Low()
+		yellowLED.High()
+		time.Sleep(time.Second)
+		yellowLED.Low()
+	}
+}
