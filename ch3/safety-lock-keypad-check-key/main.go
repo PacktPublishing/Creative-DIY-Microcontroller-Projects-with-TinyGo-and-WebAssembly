638ddd63668b50df8aee6429277beaf40ed64f45
Tobias Theel <tt@fino.digital>
2020-11-27 00:55:25 +0100

    add example

---
+++ ch3/safety-lock-keypad-check-key/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"machine"
+	"time"
+
+	"github.com/PacktPublishing/Programming-Microcontrollers-and-WebAssembly-with-TinyGo/ch3/keypad"
+	"github.com/PacktPublishing/Programming-Microcontrollers-and-WebAssembly-with-TinyGo/ch3/servo"
+)
+
+func main() {
+	keypadDriver := keypad.Driver{}
+	keypadDriver.Configure(machine.D2, machine.D3, machine.D4, machine.D5, machine.D6, machine.D7, machine.D8, machine.D9)
+
+	servoDriver := servo.Driver{}
+	servoDriver.Configure(machine.D11)
+
+	outPutConfig := machine.PinConfig{Mode: machine.PinOutput}
+
+	led1 := machine.D12
+	led1.Configure(outPutConfig)
+
+	led2 := machine.D13
+	led2.Configure(outPutConfig)
+
+	const passcode = "133742"
+	enteredPasscode := ""
+
+	for {
+		key := keypadDriver.GetKey()
+		if key != "" {
+			println("Button: ", key)
+
+			enteredPasscode += key
+
+			led2.High()
+			time.Sleep(time.Duration(time.Second / 5))
+			led2.Low()
+		}
+
+		if len(enteredPasscode) == len(passcode) {
+			if enteredPasscode == passcode {
+				println("Success")
+				enteredPasscode = ""
+				servoDriver.Right()
+
+				led1.High()
+				time.Sleep(time.Duration(time.Second * 3))
+				led1.Low()
+
+			} else {
+				println("Fail")
+				println("Entered Password: ", enteredPasscode)
+				enteredPasscode = ""
+
+				led2.High()
+				time.Sleep(time.Duration(time.Second * 3))
+				led2.Low()
+			}
+		}
+
+		time.Sleep(50 * time.Millisecond)
+	}
+}
