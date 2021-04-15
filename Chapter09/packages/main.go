98c0dce1fb0cb0dbf4206e3012f845bea12a9895
Karan Solanki <44356744+solankikaran@users.noreply.github.com>
2021-04-15 17:12:44 +0530

    Update main.go

--- Chapter09/packages/main.go
+++ Chapter09/packages/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/PacktPublishing/Programming-Microcontrollers-and-WebAssembly-with-TinyGo/Chapter-Appendix/packages/mypackage"
+import "github.com/PacktPublishing/Creative-DIY-Microcontroller-Projects-with-TinyGo-and-WebAssembly/blob/master/Chapter09/packages/mypackage/myconst.go"
 
 func main() {
 	println(mypackage.HelloWorld)
